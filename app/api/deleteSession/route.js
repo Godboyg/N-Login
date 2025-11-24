4d80dca0c374c0687351192ecb00f8757ec1417a
kushal Singh Negi <89735938+Godboyg@users.noreply.github.com>
2025-11-24 19:06:26 +0530

    Update route.js

--- app/api/deleteSession/route.js
+++ app/api/deleteSession/route.js
@@ -29,7 +29,7 @@ export async function DELETE(request) {
             return NextResponse.json({ message: "missing userAgent" }, { status: 400 });
         }
 
-        const user = await Session.findOneAndDelete({ deviceId: deviceId });
+        const user = await Session.deleteOne({ deviceId });
 
         console.log("all user deleted", user);
 
